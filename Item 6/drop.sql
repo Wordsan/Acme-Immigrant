9ce53fad88da9093faee7293219beb8ac8c580a5
Manuel Herrera Avila <dobloq@gmail.com>
2018-09-14 20:58:21 +0200

    Proyecto entregado

--- Item 6/drop.sql
+++
@@ -1,5 +0,0 @@
-use `Acmeimmigrant`;
-revoke all privileges on `Acmeimmigrant`.* from 'acme-user'@'%';
-revoke all privileges on `Acmeimmigrant`.* from 'acme-manager'@'%';
-drop user 'acme-user'@'%';
-drop user 'acme-manager'@'%';
--- Zip/Item 3/Acme-Immigrant/src/main/webapp/scripts/common.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	b = document.getElementById("errorMsg");
-	if (window.location.href.search("forbbiden.access.error") != -1) {
-		b.hidden = false;
-	} else {
-		b.hidden = true;
-	}
-});
